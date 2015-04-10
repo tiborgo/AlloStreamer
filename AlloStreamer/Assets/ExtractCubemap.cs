701311abbad27fcece339f2c999df2d80ef9e0cd
Tibor Goldschwendt <goldschwendt@cs.ucsb.edu>
2015-04-10 12:23:49 -0700

    Minor changes

--- AlloStreamer/Assets/ExtractCubemap.cs
+++ AlloStreamer/Assets/ExtractCubemap.cs
@@ -16,13 +16,13 @@ public class ExtractCubemap : MonoBehaviour {
         "LeftEye/PositiveY",
         "LeftEye/NegativeY",
         "LeftEye/PositiveZ",
-        "LeftEye/NegativeZ"/*,
+        "LeftEye/NegativeZ",
         "RightEye/PositiveX",
         "RightEye/NegativeX",
         "RightEye/PositiveY",
         "RightEye/NegativeY",
         "RightEye/PositiveZ",
-        "RightEye/NegativeZ"*/
+        "RightEye/NegativeZ"
     };
 
     private const int cubemapSize = 2048; // 1280;
