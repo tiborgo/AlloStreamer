de36f64c9cdb23be0296c6a3e467a3e11f44ef08
Tibor Goldschwendt <goldschwendt@cs.ucsb.edu>
2015-04-24 16:29:27 -0700

    Cleaned CubemapFaceSource.cpp/.h. Output stream FPS automatically adapts to render speed.

--- AlloServer/CubemapFaceSource.h
+++ AlloServer/CubemapFaceSource.h
@@ -8,87 +8,47 @@
 #include "AlloShared/CubemapFace.h"
 #include <boost/thread/condition.hpp>
 
-// The following class can be used to define specific encoder parameters
-
-class CubemapFaceSource : public FramedSource {
+class CubemapFaceSource : public FramedSource
+{
 public:
 	static CubemapFaceSource* createNew(UsageEnvironment& env, CubemapFace* face);
 
-public:
-    static EventTriggerId eventTriggerId;
-    // Note that this is defined here to be a static class variable, because this code is intended to illustrate how to
-    // encapsulate a *single* device - not a set of devices.
-    // You can, however, redefine this to be a non-static member variable.
-
 protected:
 	CubemapFaceSource(UsageEnvironment& env, CubemapFace* face);
-    // called only by createNew(), or by subclass constructors
+	// called only by createNew(), or by subclass constructors
 	virtual ~CubemapFaceSource();
 
 private:
-    // redefined virtual functions:
-    virtual void doGetNextFrame();
-    //virtual void doStopGettingFrames(); // optional
-
-private:
+	static EventTriggerId eventTriggerId;
 	static void deliverFrame0(void* clientData);
+	void deliverFrame();
 
-public:
-    void deliverFrame();
-    static void addToBuffer(uint8_t* buf, int surfaceSizeInBytes);
-
-private:
-	void frameCubemapFace();
+	// redefined virtual functions:
+	virtual void doGetNextFrame();
+	//virtual void doStopGettingFrames(); // optional
 
 	// Stores unencoded frames
-	//boost::synchronized_value<concurrent_queue<AVFrame*>> frameBuffer;
 	concurrent_queue<AVFrame*> frameBuffer;
 	// Here unused frames are stored. Included so that we can allocate all the frames at startup
 	// and reuse them during runtime
-	//boost::synchronized_value<concurrent_queue<AVFrame*>> framePool;
 	concurrent_queue<AVFrame*> framePool;
 
 	// Stores encoded frames
 	//boost::synchronized_value<concurrent_queue<AVPacket>> pktBuffer;
 	concurrent_queue<AVPacket> pktBuffer;
 
-    static unsigned referenceCount; // used to count how many instances of this class currently exist
-    //DeviceParameters fParams;
-    //AVFrame* frame;
-    //AVPacket pkt;
-    //unsigned char* randomPixels;
+	static unsigned referenceCount; // used to count how many instances of this class currently exist
 
 	CubemapFace* face;
-    //char* name;
-    AVCodecContext* codecContext;
-    int counter;
-    FILE * myfile;
-    
-    int64_t networkStartSum;
-    int64_t networkStopSum;
-    
-    int64_t networkTotalStart;
-    int64_t networkTotalStop;
+	AVCodecContext* codecContext;
 
-    void logTimes(int64_t start, int64_t stop);
-    
-	boost::mutex destructMutex;
-	boost::condition destructCondition;
+	FILE * myfile;
 
-    boost::thread encodeThread;
-	boost::thread frameThread;
-    void encodeLoop();
-    //boost::barrier encodeBarrier;
-    bool destructing;
-    unsigned int encodeSeed;
-    
-    int64_t encodeStartSumBuffer;
-    int64_t encodeStopSumBuffer;
-    int64_t encodeStartSumFrame;
-    int64_t encodeStopSumFrame;
-    int64_t encodeStartSumEncode;
-    int64_t encodeStopSumEncode;
-    int64_t encodeStartSumSync;
-    int64_t encodeStopSumSync;
-    //static const float encodeReportsPS;
+	boost::thread frameFaceThread;
+	boost::thread encodeFrameThread;
+
+	void frameFaceLoop();
+	void encodeFrameLoop();
+
+	bool destructing;
 };
