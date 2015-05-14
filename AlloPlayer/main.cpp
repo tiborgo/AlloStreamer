c5c7693f1b6593ecbd7590d55840f3bd2b882ef5
Tibor Goldschwendt <goldschwendt@cs.ucsb.edu>
2015-05-14 13:33:21 -0700

    Cleaning

--- AlloPlayer/main.cpp
+++ AlloPlayer/main.cpp
@@ -1,27 +1,15 @@
-// GLEW
-#define GLEW_STATIC
-#include <GL/glew.h>
 
-#include <iostream>
+
+
 
 int mainCubemapsSkyboxOptimized();
-int mainOmni(int argc, char* argv[]);
+int mainCubemapBackgroundApp(int argc, char* argv[]);
 int mainCubemap();
 
 int main(int argc, char* argv[])
 {
-    glewExperimental = GL_TRUE;
-
-    /*GLenum err = glewInit();
-    if (GLEW_OK != err)
-    {
-      /* Problem: glewInit failed, something is seriously wrong. */
-      /*fprintf(stderr, "Error: %s\n", glewGetErrorString(err));
-    }
-
-        std::cout << glGetString( GL_EXTENSIONS ) << std::endl;*/
 
     //mainCubemapsSkyboxOptimized();
-    mainOmni(argc, argv);
+    mainCubemapBackgroundApp(argc, argv);
     //mainCubemap();
 }
