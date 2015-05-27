7bfebbbd5272c9b98453858b0c5d15d4ac2f6318
Tibor Goldschwendt <goldschwendt@cs.ucsb.edu>
2015-05-26 17:29:18 -0700

    Removed Unity scene from repo.

---
+++ CubemapRenderingPlugin/RenderCubemap.cs
@@ -0,0 +1,131 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Runtime.InteropServices;
+using System;
+using System.Reflection;
+
+
+
+public class RenderAndExtractCubemap : MonoBehaviour {
+
+    [DllImport("CubemapExtractionPlugin")]
+    private static extern void SetCubemapFaceTextureFromUnity(System.IntPtr texture, int face);
+    [DllImport("CubemapExtractionPlugin")]
+    private static extern void StartFromUnity();
+    [DllImport("CubemapExtractionPlugin")]
+    private static extern void StopFromUnity();
+
+    private static System.String[] cubemapFaceNames = {
+        "LeftEye/PositiveX"/*,
+        "LeftEye/NegativeX",
+        "LeftEye/PositiveY",
+        "LeftEye/NegativeY",
+        "LeftEye/PositiveZ",
+        "LeftEye/NegativeZ"/*,
+        "RightEye/PositiveX",
+        "RightEye/NegativeX",
+        "RightEye/PositiveY",
+        "RightEye/NegativeY",
+        "RightEye/PositiveZ",
+        "RightEye/NegativeZ"*/
+    };
+
+    private static Vector3[] cubemapFaceRotations = {
+        new Vector3(  0,  90, 0),
+        new Vector3(  0, 270, 0),
+        new Vector3(270,   0, 0),
+        new Vector3( 90,   0, 0),
+        new Vector3(  0,   0, 0),
+        new Vector3(  0, 180, 0),
+    };
+
+    
+
+    private const int cubemapSize = /*2048;*/ 1024; // 128; // 1280;
+
+	// Use this for initialization
+    IEnumerator Start() {
+
+        // Set up 6 cameras for cubemap
+        Camera thisCam = GetComponent<Camera>();
+        GameObject cubemap = new GameObject("Cubemap");
+        cubemap.transform.parent = transform;
+        
+        // Move the cubemap to the origin of the parent cam
+        cubemap.transform.localPosition = Vector3.zero;
+
+        for (int i = 0; i < cubemapFaceNames.Length; i++)
+        {
+            GameObject go = new GameObject(cubemapFaceNames[i]);
+            Camera cam = go.AddComponent<Camera>()/*.GetCopyOf(thisCam)*/;
+
+            //GameObject go = GameObject.Find(cameraNames[i]);
+            //Camera cam = go.GetComponent<Camera>();
+
+            // Set render texture
+            RenderTexture tex = new RenderTexture(cubemapSize, cubemapSize, 1, RenderTextureFormat.ARGB32);
+            tex.Create();
+            cam.targetTexture = tex;
+            cam.aspect = 1;
+
+            // Set orientation
+            cam.fieldOfView = 90;
+            go.transform.eulerAngles = cubemapFaceRotations[i];
+            go.transform.parent = cubemap.transform;
+
+			// Move the cubemap to the origin of the parent cam
+			cam.transform.localPosition = Vector3.zero;
+
+            SetCubemapFaceTextureFromUnity(tex.GetNativeTexturePtr(), i);
+        }
+
+        // Tell native plugin that rendering has started
+        StartFromUnity();
+
+        yield return StartCoroutine("CallPluginAtEndOfFrames");
+	}
+
+    void OnDestroy()
+    {
+        StopFromUnity();
+    }
+
+
+    private IEnumerator CallPluginAtEndOfFrames()
+    {
+        while (true)
+        {
+            yield return new WaitForEndOfFrame();
+            GL.IssuePluginEvent(1);
+        }
+    }
+}
+
+public static class MyExtensions
+{
+    // From http://answers.unity3d.com/questions/530178/how-to-get-a-component-from-an-object-and-add-it-t.html
+    public static T GetCopyOf<T>(this Component comp, T other) where T : Component
+    {
+        Type type = comp.GetType();
+        if (type != other.GetType()) return null; // type mis-match
+        BindingFlags flags = BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Default | BindingFlags.DeclaredOnly;
+        PropertyInfo[] pinfos = type.GetProperties(flags);
+        foreach (var pinfo in pinfos)
+        {
+            if (pinfo.CanWrite)
+            {
+                try
+                {
+                    pinfo.SetValue(comp, pinfo.GetValue(other, null), null);
+                }
+                catch { } // In case of NotImplementedException being thrown. For some reason specifying that exception didn't seem to catch it, so I didn't catch anything specific.
+            }
+        }
+        FieldInfo[] finfos = type.GetFields(flags);
+        foreach (var finfo in finfos)
+        {
+            finfo.SetValue(comp, finfo.GetValue(other));
+        }
+        return comp as T;
+    }
+}
