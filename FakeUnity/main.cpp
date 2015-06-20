4d6ab8cacc07f6f6f2b79502727343fa51f8abf7
Tibor Goldschwendt <goldschwendt@cs.ucsb.edu>
2015-06-19 19:03:03 -0700

    Implemented helper program that simulates Unity for machines that cannot run Unity scenes (e.g. because they have no screen)

---
+++ FakeUnity/main.cpp
@@ -0,0 +1,128 @@
+#include <math.h>
+#include <stdio.h>
+#include <boost/thread.hpp>
+#include <boost/interprocess/managed_shared_memory.hpp>
+#include <boost/interprocess/offset_ptr.hpp>
+
+#include "AlloShared/config.h"
+#include "AlloShared/Process.h"
+#include "AlloServer/AlloServer.h"
+#include "AlloShared/Cubemap.hpp"
+#include "CubemapExtractionPlugin/CubemapExtractionPlugin.h"
+
+static ShmAllocator* shmAllocator = nullptr;
+static Process* thisProcess = nullptr;
+static Process alloServerProcess(ALLOSERVER_ID, false);
+static boost::chrono::system_clock::time_point presentationTime;
+
+boost::interprocess::managed_shared_memory shm;
+
+void allocateSHM(int facesCount, int resolution)
+{
+    boost::interprocess::shared_memory_object::remove(SHM_NAME);
+    
+    shm =
+    boost::interprocess::managed_shared_memory(boost::interprocess::open_or_create,
+                                               SHM_NAME,
+                                               resolution * resolution * 4 * facesCount +
+                                               sizeof(Cubemap) +
+                                               facesCount * sizeof(CubemapFace) +
+                                               65536);
+    
+    shmAllocator = new ShmAllocator(*(new ShmAllocator::BoostShmAllocator(shm.get_segment_manager())));
+    
+    shm.destroy<Cubemap::Ptr>("Cubemap");
+    cubemap = nullptr;
+}
+
+void allocateCubemap(std::vector<CubemapFace*>& faces)
+{
+    cubemap = Cubemap::create(faces, *shmAllocator);
+    Cubemap::Ptr cubemapPtr = *shm.construct<Cubemap::Ptr>("Cubemap")(cubemap.get());
+}
+
+void releaseSHM()
+{
+    boost::interprocess::shared_memory_object::remove(SHM_NAME);
+    cubemap = nullptr;
+}
+
+int main(int argc, char* argv[])
+{
+    if (argc < 3)
+    {
+        std::cout << "FakeUnity <faces count> <face resolution>" << std::endl;
+        return -1;
+    }
+    
+    int cubemapFacesCount = atoi(argv[1]);
+    int resolution = atoi(argv[2]);
+    presentationTime = boost::chrono::system_clock::now();
+    
+    // Create and/or open shared memory
+    allocateSHM(cubemapFacesCount, resolution);
+    std::vector<CubemapFace*> faces;
+    for (int i = 0; i < cubemapFacesCount; i++)
+    {
+        faces.push_back(CubemapFace::create(resolution,
+                                            resolution,
+                                            i,
+                                            AV_PIX_FMT_RGB24,
+                                            presentationTime,
+                                            *shmAllocator));
+    }
+    allocateCubemap(faces);
+    
+    thisProcess = new Process(CUBEMAPEXTRACTIONPLUGIN_ID, true);
+    
+    std::cout << "Faking Unity :p (cubemap " << cubemapFacesCount << "x" << resolution << "x" << resolution << ")" << std::endl;
+    
+    boost::chrono::system_clock::time_point lastTime = boost::chrono::system_clock::now();
+    int frames = 0;
+    boost::chrono::milliseconds fpsMeasurementPeriod(5000);
+    
+    while(true)
+    {
+        for (CubemapFace* face : faces)
+        {
+            while (!face->getMutex().timed_lock(boost::get_system_time() + boost::posix_time::milliseconds(100)))
+            {
+                if (!alloServerProcess.isAlive())
+                {
+                    // Reset the mutex otherwise it will block forever
+                    // Reset the condition otherwise it my be in inconsistent state
+                    // Hacky solution indeed
+                    void* mutexAddr     = &face->getMutex();
+                    void* conditionAddr = &face->getNewPixelsCondition();
+                    // That's not possible unfortunately since the mutex is locked and abandoned
+                    //face->getMutex().~interprocess_mutex();
+                    memset(mutexAddr, 0, sizeof(boost::interprocess::interprocess_mutex));
+                    memset(conditionAddr, 0, sizeof(boost::interprocess::interprocess_condition));
+                    new (mutexAddr)     boost::interprocess::interprocess_mutex;
+                    new (conditionAddr) boost::interprocess::interprocess_condition;
+                }
+            }
+            
+            boost::interprocess::scoped_lock<boost::interprocess::interprocess_mutex> lock(face->getMutex(),
+                                                                                           boost::interprocess::accept_ownership);
+            face->getNewPixelsCondition().notify_all();
+        }
+        frames++;
+        
+        boost::chrono::system_clock::time_point now = boost::chrono::system_clock::now();
+        boost::chrono::milliseconds period =  boost::chrono::duration_cast<boost::chrono::milliseconds>(now - lastTime);
+        
+        if (period >= fpsMeasurementPeriod)
+        {
+            double fps = frames * 1000.0 / period.count();
+            std::cout << "Current FPS is " << fps << std::endl;
+            frames = 0;
+            lastTime = now;
+        }
+    }
+    
+    delete thisProcess;
+    releaseSHM();
+    
+    return 0;
+}
