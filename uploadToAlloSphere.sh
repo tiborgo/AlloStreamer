8ab638bfa98fd8c1802b2ebcfdc23f7e07caa3cb
Tibor Goldschwendt <goldschwendt@cs.ucsb.edu>
2015-06-22 16:39:27 -0700

    Script for uploading compilations to AlloSphere can distinguish between the server and the player part.

--- uploadToAlloSphere.sh
+++ uploadToAlloSphere.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
+os=$(uname -s)
 dir=$(dirname "$(readlink -f "$0")")
-scp -r ${dir}/Bin/* nonce:AlloUnity/Bin/
-ssh -t nonce bash -c "'
-	AlloUnity/uploadToAlloSphere.sh
-'"	
+if [ "${os}" = "Linux" ]
+then
+	echo "Uploading to AlloSphere rendering machines ..."
+	scp -r ${dir}/Bin/* nonce:AlloUnity/Bin/ > /dev/null
+	ssh -t nonce bash -c "'
+		AlloUnity/uploadToAlloSphere.sh player
+	'" > /dev/null
+	echo "Done!"
+elif [ "${os}" = "Darwin" ]
+then
+	echo "Uploading to audio Mac ..."
+	scp -r ${dir}/Bin/* nonce:AlloUnity/Mac/Bin/ > /dev/null
+	ssh -t nonce bash -c "'
+		AlloUnity/uploadToAlloSphere.sh server
+	'" > /dev/null
+	echo "Done!"
+else
+	echo "Cannot upload ${os} compilations."
+fi
