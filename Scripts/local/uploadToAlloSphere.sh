b9a07e82d70533c6b9fb34815777c8518d5a24d6
Tibor Goldschwendt <goldschwendt@cs.ucsb.edu>
2015-06-22 19:16:29 -0700

    Fixed upload script on Mac

--- Scripts/local/uploadToAlloSphere.sh
+++ Scripts/local/uploadToAlloSphere.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
+
+if [ -x "$(command -v greadlink)" ]; then
+	dir=$(dirname "$(greadlink -f "$0")")
+else
+	dir=$(dirname "$(readlink -f "$0")")
+fi
+
 os=$(uname -s)
-dir=$(dirname "$(readlink -f "$0")")
-if [ "${os}" = "Linux" ]
-then
+
+if [ "${os}" = "Linux" ]; then
 	echo "Uploading to AlloSphere rendering machines ..."
 	scp -r ${dir}/../../Bin/* nonce:AlloUnity/Bin/ > /dev/null
 	ssh -t nonce bash -c "'
 		AlloUnity/uploadToAlloSphere.sh player
 	'" > /dev/null
 	echo "Done!"
-elif [ "${os}" = "Darwin" ]
-then
+elif [ "${os}" = "Darwin" ]; then
 	echo "Uploading to audio Mac ..."
 	scp -r ${dir}/../../Bin/* nonce:AlloUnity/Mac/Bin/ > /dev/null
 	ssh -t nonce bash -c "'
 		AlloUnity/uploadToAlloSphere.sh server
-	'" > /dev/null
+	'"
 	echo "Done!"
 else
 	echo "Cannot upload ${os} compilations."
