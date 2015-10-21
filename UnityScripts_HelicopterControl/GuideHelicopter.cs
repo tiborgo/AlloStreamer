1a77bbbf3c0634399cb4869309225de0ab7568dc
Jahwffrey <johnthomason@umail.ucsb.edu>
2015-10-20 21:28:11 -0700

    Another attempt, still doing something wrong

---
+++ UnityScripts_HelicopterControl/GuideHelicopter.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using System.Collections;
+using System.Runtime.InteropServices;
+/// MouseLook rotates the transform based on the mouse delta.
+/// Minimum and Maximum values can be used to constrain the possible rotation
+
+public class GuideHelicopter : MonoBehaviour
+{
+
+    
+    void Update()
+    {
+		float test = getStartX ();
+    }
+
+    void Start()
+    {
+		
+
+    }
+}
